a61576b0618826535e43854922ea390b7e2aafce
Sarvesh S. <75280669+Sarvesh-Eng@users.noreply.github.com>
2025-02-18 20:37:21 -0800

    Update EmployeeLowAttendanceIndicator.sql

--- EmployeeLowAttendanceIndicator.sql
+++ EmployeeLowAttendanceIndicator.sql
@@ -1,4 +1,4 @@
 SELECT Employee_ID, Name, Department, Attendance_Days
-FROM `erudite-marker-451401-g4.Employee_Data.Employee_Management`
+FROM `Project.Employee_Data.Employee_Management`
 WHERE Attendance_Days < 20
 ORDER BY Attendance_Days ASC;
