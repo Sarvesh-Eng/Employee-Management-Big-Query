dfc0005e3dbd400eb379e05479acd26312d65049
Sarvesh S. <75280669+Sarvesh-Eng@users.noreply.github.com>
2025-02-18 20:37:46 -0800

    Update EmployeeBestPerformance.sql

--- EmployeeBestPerformance.sql
+++ EmployeeBestPerformance.sql
@@ -1,4 +1,4 @@
 SELECT Employee_ID, Name, Performance_Score
-FROM `erudite-marker-451401-g4.Employee_Data.Employee_Management`
+FROM `Project.Employee_Data.Employee_Management`
 ORDER BY Performance_Score DESC
 LIMIT 100;
